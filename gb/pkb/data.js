8f3a766117b32658593ed769f008033149b7a186
Ethan <68365423+letsgoawaydev@users.noreply.github.com>
2025-07-04 10:44:58 +0800

    update

---
+++ gb/pkb/data.js
@@ -0,0 +1,57 @@
+EJS_gameName = "pkb";
+EJS_core = "gb";
+//EJS_DEBUG_XX = true;
+EJS_defaultOptions = {
+
+    //"menu-bar-button": "hidden"
+}
+
+
+EJS_pathtodata = "/app-ejs/data/";
+EJS_player = "#game";
+EJS_color = "#242424";
+EJS_backgroundColor = "#000";
+EJS_alignStartButton = "center";
+EJS_startOnLoaded = true;
+EJS_Buttons = {
+    playPause: {
+        visible: false,
+    },
+    cheat: {
+        visible: false,
+    },
+    screenshot: {
+        visible: false,
+    },
+    loadState: {
+        visible: false,
+    },
+    saveState: {
+        visible: false,
+    },
+    quickSave: {
+        visible: false,
+    },
+    quickLoad: {
+        visible: false,
+    },
+    screenRecord: {
+        visible: false,
+    },
+    cacheManager: false,
+    exitEmulation: false,
+    settings: false,
+    fullscreen: false,
+    contextMenu: false,
+};
+EJS_biosUrl = ["/app-ejs/gb/gb_bios.bin", "/app-ejs/gb/gbc_bios.bin"];
+EJS_gameUrl = "/app-ejs/gb/pkb/pkb.data";
+
+window.addEventListener("mousemove", (ev) => {
+    let menubar = document.getElementsByClassName("ejs_menu_bar").item(0);
+    if (menubar != null && !menubar.classList.contains("ejs_menu_bar_hidden")) {
+        if (window.disableEmulatorMenu == true) {
+            menubar.classList.add("ejs_menu_bar_hidden");
+        }
+    }
+})
